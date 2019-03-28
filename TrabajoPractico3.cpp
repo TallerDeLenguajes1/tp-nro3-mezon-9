ce243fb9be9125b76d6bf2da21a73744d8306262
Alumno <franck_l_m@hotmail.com.ar>
2019-03-28 18:33:22 -0300

    codigo base

---
+++ TrabajoPractico3.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct TDatos {
+	TRaza Raza; //nota 1
+	Char *ApellidoNombre; //nota 2
+	int edad; //entre 0 a 300
+	double Salud://100
+};
+	struct TCaracteristicas{
+	int velocidad;// 1 a 10
+	int destreza; //1 a 5
+	int fuerza;//1 a 10
+	int Nivel; //1 a 10
+	int Armadura; //1 a 10
+};
+	struct TPersonaje {
+	TDatos * DatosPersonales
+	TCaracteristicas * Caracteristicas
+};
+
+int main(){
+
+
+	return 0;
+}
