7b18f3b5a7daa00c2ed5c5814c0e0f4764e26154
Alejandro <ale_arc2015@hotmail.com>
2019-04-04 17:16:01 -0300

    funciones cargar y mostrar datos

--- TrabajoPractico3.cpp
+++ TrabajoPractico3.cpp
@@ -1,27 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 
-
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+char Nombres[6][10]={"juan","esteban","armando","augusto","gian","maxi"};
+char Apellidos[6][10]={"fasito","quito","sneider","servimoto","casa","rifa"};
 struct TDatos {
 	TRaza Raza; //nota 1
-	Char *ApellidoNombre; //nota 2
+	char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
-	double Salud://100
+	double Salud;//100
 };
-	struct TCaracteristicas{
+struct TCaracteristicas{
 	int velocidad;// 1 a 10
 	int destreza; //1 a 5
 	int fuerza;//1 a 10
 	int Nivel; //1 a 10
 	int Armadura; //1 a 10
 };
-	struct TPersonaje {
-	TDatos * DatosPersonales
-	TCaracteristicas * Caracteristicas
+struct TPersonaje {
+	TDatos * DatosPersonales;
+	TCaracteristicas * Caracteristicas;
 };
-
-int main(){
-
-
+void CargarDatos(TPersonaje *dato, TRaza raza,char *nombre ,char *apellido,int edad,double salud);
+void MostrarDatos(TPersonaje *D);
+int main()
+{
+	TPersonaje *D;
+	char *n,*a;
+	n="Juan";
+	a="Rodriguez";
+	D=(TPersonaje *)malloc(sizeof(TPersonaje));
+	printf("**********************************\n");
+	CargarDatos(D,Mago,n,a,200,50);
+	MostrarDatos(D);
 	return 0;
+}
+void CargarDatos(TPersonaje *dato,TRaza raza,char *nombre ,char *apellido,int edad,double salud){
+	
+	dato->DatosPersonales=(TDatos *)malloc(sizeof(TDatos));
+//	dato->DatosPersonales->ApellidoNombre=(char *)malloc(sizeof(char)*(strlen(nombre)+strlen(apellido)));
+	dato->DatosPersonales->ApellidoNombre=(char *)malloc(sizeof(char)*(strlen(nombre)+strlen(apellido)+1));
+	dato->DatosPersonales->ApellidoNombre=strcat(apellido," ");
+	dato->DatosPersonales->ApellidoNombre=strcat(dato->DatosPersonales->ApellidoNombre,nombre);
+	dato->DatosPersonales->Raza=raza;
+	dato->DatosPersonales->edad=edad;
+	dato->DatosPersonales->Salud=salud;
+}
+
+void MostrarDatos(TPersonaje *D){
+	
+	printf("Apellido Y Nombre: %s\n", D->DatosPersonales->ApellidoNombre);
+	printf("Raza: %d\n", D->DatosPersonales->Raza);
+	printf("Edad: %d\n",D->DatosPersonales->edad );
+	printf("Salud: %.2f\n", D->DatosPersonales->Salud);
 }
