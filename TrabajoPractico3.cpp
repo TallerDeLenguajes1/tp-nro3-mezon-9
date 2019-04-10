5a9ba2eb81a48be259692be2d8c86f6431ff641a
Mario <mariokpo10@gmail.com>
2019-04-10 12:45:26 -0300

    Subiendo punto 3

--- TrabajoPractico3.cpp
+++ TrabajoPractico3.cpp
@@ -1,91 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
-#include <conio.h>
 
-enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+enum TRaza{Orco = 1, Humano, Mago, Enano, Elfo};
 char Nombres[6][10]={"juan","esteban","armando","augusto","gian","maxi"};
 char Apellidos[6][10]={"fasito","quito","sneider","servimoto","casa","rifa"};
+
 struct TDatos {
 	TRaza Raza; //nota 1
 	char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
 	double Salud;//100
 };
-struct TCaracteristicas{
+	struct TCaracteristicas{
 	int velocidad;// 1 a 10
 	int destreza; //1 a 5
 	int fuerza;//1 a 10
 	int Nivel; //1 a 10
 	int Armadura; //1 a 10
 };
-struct TPersonaje {
+	struct TPersonaje {
 	TDatos * DatosPersonales;
 	TCaracteristicas * Caracteristicas;
 };
+void cargar_datos(struct TDatos *datos);
+void mostrar_datos(TDatos *datos);
+void cargarCaracteristicas(TCaracteristicas *cargar);
+void mostrarCaracterist(TCaracteristicas *mostrar);
 
-void CargarDatos(TPersonaje *dato, TRaza raza,char *nombre ,char *apellido,int edad,double salud);
-void MostrarDatos(TPersonaje *D);
-void cargar_caract(TCaracteristicas *p);
+int main(){
+	srand (time(NULL));
 
-int main()
-{
-	TPersonaje *D;
-	TCaracteristicas p;
-	//char *n,*a;
-	char n[100]="Juan",a[100]="Rodriguez";
-	/*printf("Ingrese nombre: ");
-	scanf("%s",&n);
-	printf("Ingrese apellido: ");
-	scanf("%s",&a);
-	printf("\n");*/
-//	n="Juan";
-//	a="Rodriguez";
-	D=(TPersonaje *)malloc(sizeof(TPersonaje));
-	//printf("**********************************\n");
-	CargarDatos(D,Mago,n,a,200,50);
-	MostrarDatos(D);
-	printf("\n\n");
+	//---------------------------------------------------------------//	
+	TDatos *datos;
+	datos=(TDatos*)malloc(sizeof(TDatos));
 
-	cargar_caract(&p);
-	//printf("**********************************\n");
+	cargar_datos(datos);
+	mostrar_datos(datos);
+	//-------------------------------------------------------------//
+	TCaracteristicas *caract;
+
+	caract=(TCaracteristicas*)malloc(sizeof(TCaracteristicas));
+	cargarCaracteristicas(caract);
+	mostrarCaracterist(caract);
+	//--------------------------------------------------------------//
+	printf("\n---------------------------------\n");
+	int n;
+	printf("Ingrese el numero de jugadores: ");
+	scanf("%d", &n);
+	printf("\n---------------------------------\n");
+
+	for (int i = 0; i < n; ++i)
+	{
+		printf("Datos del jugador numero: %d\n",i+1 );
+		TDatos *datos;
+		datos=(TDatos*)malloc(sizeof(TDatos));
+
+		cargar_datos(datos);
+		mostrar_datos(datos);
+		caract=(TCaracteristicas*)malloc(sizeof(TCaracteristicas));
+		cargarCaracteristicas(caract);
+		mostrarCaracterist(caract);
+		printf("---------------------------------\n");
+
+	}
+
+
 	return 0;
 }
-void CargarDatos(TPersonaje *dato,TRaza raza,char *nombre ,char *apellido,int edad,double salud){
-	
-	dato->DatosPersonales=(TDatos *)malloc(sizeof(TDatos));
-//	dato->DatosPersonales->ApellidoNombre=(char *)malloc(sizeof(char)*(strlen(nombre)+strlen(apellido)));
-	dato->DatosPersonales->ApellidoNombre=(char *)malloc(sizeof(char)*(strlen(nombre)+strlen(apellido)+2));
-	dato->DatosPersonales->ApellidoNombre=strcat(apellido," ");
-	dato->DatosPersonales->ApellidoNombre=strcat(dato->DatosPersonales->ApellidoNombre,nombre);
-	dato->DatosPersonales->ApellidoNombre=strcat(dato->DatosPersonales->ApellidoNombre,"\0");
-	//printf("%s\n", dato->DatosPersonales->ApellidoNombre);
-	dato->DatosPersonales->Raza=raza;
-	dato->DatosPersonales->edad=edad;
-	dato->DatosPersonales->Salud=salud;
+void cargarCaracteristicas(TCaracteristicas *cargar)
+{
+	cargar->velocidad = 1 + rand()%11-1;	
+	cargar->destreza = 1 + rand()%6-1;
+	cargar->fuerza = 1 +rand()%11-1;
+	cargar->Nivel = 1 + rand()%11-1;
+	cargar->Armadura = 1 + rand()%11-1;
+}
+void mostrarCaracterist(TCaracteristicas *mostrar)
+{
+	printf("Velocida: %d\n",mostrar->velocidad);
+	printf("Destreza: %d\n",mostrar->destreza);
+	printf("fueza: %d\n",mostrar->fuerza);
+	printf("Nivel: %d\n",mostrar->Nivel);
+	printf("Armadura: %d\n",mostrar->Armadura);
 }
 
-void MostrarDatos(TPersonaje *D){
-	
-	printf("Apellido Y Nombre: %s\n", D->DatosPersonales->ApellidoNombre);
-	printf("Raza: %d\n", D->DatosPersonales->Raza);
-	printf("Edad: %d\n",D->DatosPersonales->edad );
-	printf("Salud: %.2f\n", D->DatosPersonales->Salud);
+void cargar_datos(struct TDatos *datos)
+{
+	datos->ApellidoNombre = (char *) malloc (10);
+	strcpy(datos->ApellidoNombre, Nombres[0 + rand()% (6-1)]);
+	printf("Nombre y Apellido: %s ", datos->ApellidoNombre );
+
+	strcpy(datos->ApellidoNombre, Apellidos[0 + rand()% (6-1)]);
+	printf("%s\n", datos->ApellidoNombre );
+
+	datos->edad = 0 + rand()%(301-1);
+	datos->Salud = 100;
+
 }
 
-void cargar_caract(TCaracteristicas *p){
+void mostrar_datos(TDatos *datos)
+{
+	int Raza = 1+rand()%(6-1);
 
-	srand(time(NULL));
-	p -> velocidad=1 + rand()%(1+9);
-	p -> destreza=1 + rand()%(1+5);
-	p -> fuerza =1 + rand()%(1+9);
-	p -> Nivel=1 + rand()%(1+9);
-	p -> Armadura=1 + rand()%(1+9);
+	switch(Raza){
+		case 1: printf("Raza: Orco\n");
+			break;
+		case 2: printf("Raza: Humano\n");
+			break;
+		case 3: printf("Raza: Mago\n");
+			break;
+		case 4: printf("Raza: Enano\n");
+			break;
+		case 5: printf("Raza: Elfo\n");
+			break;
+	}
+	//printf("Nombre y Apellido: %s\n", datos->ApellidoNombre );
+	printf("Edad: %d\n", datos->edad);
+	printf("Salud: %2.f\n",datos->Salud );
 
-	printf("velocidad=%d\n\r",p -> velocidad);
-	printf("destreza=%d\n\r",p -> destreza);
-	printf("fuerza=%d\n\r",p -> fuerza);
-	printf("nivel=%d\n\r",p -> Nivel);
-	printf("armadura=%d\n\r",p -> Armadura);
-	
 }
