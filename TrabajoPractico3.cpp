5bffa19107b680350b6fee820ae1bac09bcb0e4b
Augusto <you@example.com>
2019-04-14 13:16:50 -0300

    gameplay listo

--- TrabajoPractico3.cpp
+++ TrabajoPractico3.cpp
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include <time.h>
 
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 char Nombres[6][10]={"juan","esteban","armando","augusto","gian","maxi"};
-char Apellidos[6][10]={"fasito","quito","sneider","servimoto","casa","rifa"};
+char Apellidos[6][10]={"fasito","quito","sneider","servimoto","Soul","rifa"};
 struct TDatos {
 	TRaza Raza; //nota 1
 	char *ApellidoNombre; //nota 2
@@ -24,68 +25,181 @@ struct TPersonaje {
 	TCaracteristicas * Caracteristicas;
 };
 
-void CargarDatos(TPersonaje *dato, TRaza raza,char *nombre ,char *apellido,int edad,double salud);
+void CargarDatos(TPersonaje *dato);
 void MostrarDatos(TPersonaje *D);
-void cargar_caract(TCaracteristicas *p);
-
+void cargar_caract(TPersonaje *D);
+void Mostrar_caract(TPersonaje *D);
+void game(TPersonaje *D1,TPersonaje *D2);
 int main()
 {
 	TPersonaje *D;
-	TCaracteristicas p;
-	//char *n,*a;
-	char n[100]="Juan",a[100]="Rodriguez";
-	/*printf("Ingrese nombre: ");
-	scanf("%s",&n);
-	printf("Ingrese apellido: ");
-	scanf("%s",&a);
-	printf("\n");*/
-//	n="Juan";
-//	a="Rodriguez";
-	D=(TPersonaje *)malloc(sizeof(TPersonaje));
+	srand(time(NULL));
+	
+	int P1,P2;
+	
+	
+	D=(TPersonaje *)malloc(sizeof(TPersonaje)*5);
 	//printf("**********************************\n");
-	CargarDatos(D,Mago,n,a,200,50);
-	MostrarDatos(D);
-	printf("\n\n");
+	for (int i = 0; i < 5; i++)
+	{
+		// l=rand()%5;
+		// k=rand()%5;
+		// n= (char *)malloc(sizeof(char)*(strlen(Nombres[l])));
+		// a= (char *)malloc(sizeof(char)*(strlen(Apellidos[k])));
+		// n=strcat(Nombres[l],"\0");
+		// a=strcat(Apellidos[k],"\0");
+		// strcpy(n,Nombres[l]);
+			CargarDatos (D+i);
+			cargar_caract(D+i);
 
-	cargar_caract(&p);
-	//printf("**********************************\n");
+	}
+	for (int i = 0; i < 5; i++)
+	{
+
+		printf("\nPersonaje Numero %d\n",i+1 );		
+		MostrarDatos(D+i);
+		printf("**************************************************\n");
+		printf("Caracteristicas:\n");
+		Mostrar_caract(D+i);
+		printf("**************************************************\n");
+	}
+	printf("\n\n");
+	printf("Player1 elija su personaje al escribir su numero: \n");
+	scanf("%d",&P1);
+	printf("Player2 elija su personaje al escribir su numero: \n");
+	scanf("%d",&P2);
+	game(D+(P1-1),D+(P2-1));
+	//cargar_caract(&p);
+	free(D);
 	return 0;
 }
-void CargarDatos(TPersonaje *dato,TRaza raza,char *nombre ,char *apellido,int edad,double salud){
+void CargarDatos(TPersonaje *dato){
+	
+	int a = rand()%(5);
+	int b = rand()%(5);
 	
 	dato->DatosPersonales=(TDatos *)malloc(sizeof(TDatos));
-//	dato->DatosPersonales->ApellidoNombre=(char *)malloc(sizeof(char)*(strlen(nombre)+strlen(apellido)));
-	dato->DatosPersonales->ApellidoNombre=(char *)malloc(sizeof(char)*(strlen(nombre)+strlen(apellido)+2));
-	dato->DatosPersonales->ApellidoNombre=strcat(apellido," ");
-	dato->DatosPersonales->ApellidoNombre=strcat(dato->DatosPersonales->ApellidoNombre,nombre);
-	dato->DatosPersonales->ApellidoNombre=strcat(dato->DatosPersonales->ApellidoNombre,"\0");
-	//printf("%s\n", dato->DatosPersonales->ApellidoNombre);
-	dato->DatosPersonales->Raza=raza;
-	dato->DatosPersonales->edad=edad;
-	dato->DatosPersonales->Salud=salud;
+
+	dato->DatosPersonales->ApellidoNombre = (char *)malloc(sizeof(char)*(strlen(Apellidos[b]) + strlen(Nombres[a]) + 1));
+	strcpy(dato->DatosPersonales->ApellidoNombre, Apellidos[b]);
+	strcat(dato->DatosPersonales->ApellidoNombre, " ");
+	strcat(dato->DatosPersonales->ApellidoNombre, Nombres[a]);
+	
+	dato->DatosPersonales->edad = rand()%300;
+	dato->DatosPersonales->Salud = 100;
 }
 
 void MostrarDatos(TPersonaje *D){
 	
 	printf("Apellido Y Nombre: %s\n", D->DatosPersonales->ApellidoNombre);
-	printf("Raza: %d\n", D->DatosPersonales->Raza);
+	printf("Raza: ");
+	switch((D->DatosPersonales->Raza)+1){
+		case 1:{
+			printf("Orco\n");
+			break;
+		}
+		case 2:{
+			printf("Humano\n");
+			break;
+		}
+		case 3:{
+			printf("Mago\n");
+			break;
+		}
+		case 4:{
+			printf("Enano\n");
+			break;
+		}
+		case 5:{
+			printf("Elfo\n");
+			break;
+		}
+	}
 	printf("Edad: %d\n",D->DatosPersonales->edad );
-	printf("Salud: %.2f\n", D->DatosPersonales->Salud);
+	printf("Salud: %.2f\n\n", D->DatosPersonales->Salud);
 }
 
-void cargar_caract(TCaracteristicas *p){
-
-	srand(time(NULL));
-	p -> velocidad=1 + rand()%(1+9);
-	p -> destreza=1 + rand()%(1+5);
-	p -> fuerza =1 + rand()%(1+9);
-	p -> Nivel=1 + rand()%(1+9);
-	p -> Armadura=1 + rand()%(1+9);
-
-	printf("velocidad=%d\n\r",p -> velocidad);
-	printf("destreza=%d\n\r",p -> destreza);
-	printf("fuerza=%d\n\r",p -> fuerza);
-	printf("nivel=%d\n\r",p -> Nivel);
-	printf("armadura=%d\n\r",p -> Armadura);
+void cargar_caract(TPersonaje *D){
 	
+	
+
+	D->Caracteristicas=(TCaracteristicas *)malloc(sizeof(TCaracteristicas ));
+	D->Caracteristicas->velocidad = 1 + rand()%(1+9);
+	D->Caracteristicas->destreza = 1 + rand()%(1+4);
+	D->Caracteristicas->fuerza = 1 + rand()%(1+9);
+	D->Caracteristicas->Nivel = 1 + rand()%(1+9);
+	D->Caracteristicas->Armadura = 1 + rand()%(1+9);
+}
+void Mostrar_caract(TPersonaje *D){
+	printf("velocidad=%d\n\r",D->Caracteristicas->velocidad);
+	printf("destreza=%d\n\r",D->Caracteristicas->destreza);
+	printf("fuerza=%d\n\r",D->Caracteristicas->fuerza);
+	printf("nivel=%d\n\r",D->Caracteristicas->Nivel);
+	printf("armadura=%d\n\r",D->Caracteristicas->Armadura);
+	
+}
+void game(TPersonaje *D1,TPersonaje *D2){
+	printf("\n\tPlayer1:\n");
+	MostrarDatos(D1);
+	Mostrar_caract(D1);
+
+	printf("\n\n\tPlayer2:\n");
+	MostrarDatos(D2);
+	Mostrar_caract(D2);
+	printf("\n\nPresione una tecla para la batalla  mas epica q hayas visto en una juego...");
+	getch();
+
+	for (int i = 0; i <3 && D2->DatosPersonales->Salud > 0 && D1->DatosPersonales->Salud > 0; i++)
+	{
+		double PD1, PD2, ED1, ED2, VA1, VA2, PDEF1, PDEF2, DP1, DP2, MDP;
+		MDP=50000;
+		PD1=D1->Caracteristicas->destreza*D1->Caracteristicas->fuerza*D1->Caracteristicas->Nivel;
+		ED1=rand()%(1+99);
+		VA1=PD1*ED1;
+		PDEF1=D1->Caracteristicas->Armadura*D1->Caracteristicas->velocidad;
+		PD2=D2->Caracteristicas->destreza*D2->Caracteristicas->fuerza*D2->Caracteristicas->Nivel;
+		ED2=rand()%(1+99);
+		VA2=PD2*ED2;
+		PDEF2=D2->Caracteristicas->Armadura*D2->Caracteristicas->velocidad;
+		printf("\n\nPresione una tecla para que ataque Player1");
+		getch();
+		DP1=((VA1-PDEF2)/MDP)*100;
+		D2->DatosPersonales->Salud = D2->DatosPersonales->Salud - DP1;
+		printf("Ataque Player1 quita a player 2: '%.2f' puntos de vida\n", DP1);
+		if (D2->DatosPersonales->Salud <= 0)
+		{
+		printf("Salud de Player2: '0'\n");
+		break;
+			
+		}else{
+		printf("Salud de Player2: '%.2f'\n", D2->DatosPersonales->Salud);			
+		}
+		printf("\n\nPresione una tecla para que ataque Player2");
+		getch();
+		DP2=((VA2-PDEF1)/MDP)*100;
+		D1->DatosPersonales->Salud = D1->DatosPersonales->Salud - DP2;
+		printf("Ataque Player2 quita a Player 1: '%.2f' puntos de vida\n", DP2);
+
+		if (D1->DatosPersonales->Salud <= 0)
+		{
+		printf("Salud de Player1: '0'");
+		break;
+		}else{
+		printf("Salud de Player2: '%.2f'\n", D1->DatosPersonales->Salud);
+		}
+	
+	}
+	if (D2->DatosPersonales->Salud > D1->DatosPersonales->Salud)
+		{
+			printf("\n\t'----****Ganador Player2****---'\n");
+		}
+		else{
+			if (D2->DatosPersonales->Salud == D1->DatosPersonales->Salud)
+			{
+				printf("\n\t'----****EMPATE****---'\n");
+			}else{
+				printf("\n\t'----****Ganador Player1****---'\n");
+			}
+		}
+		
 }
